cf2051ac9e14e1daab4c252b0a1e2a134a23f522
Marco Bonetti <marco@cruncher.ch>
2026-09-17 15:23:59 +0200

    admin

--- hoard/apps/hoarder/admin.py
+++ hoard/apps/hoarder/admin.py
@@ -1,11 +1,51 @@
+import json
+
 from django.contrib import admin
+from django.utils.safestring import SafeString
+from pygments import highlight
+from pygments.formatters import HtmlFormatter
+from pygments.lexers import JsonLexer
 
 from .models import Category, Header, Hoard
 
 
 @admin.register(Hoard)
 class HoardAdmin(admin.ModelAdmin):
-    list_display = ("category", "recorded", "data")
+    list_display = ("category", "recorded", "data_excerpt")
+
+    readonly_fields = ("pretty_json",)
+
+    @admin.display(description="Data")
+    def pretty_json(self, instance):
+        """Function to display pretty version of our data"""
+
+        # Convert the data to sorted, indented JSON
+        response = json.dumps(
+            instance.data, sort_keys=True, indent=2
+        )  # <-- your field here
+
+        # Truncate the data. Alter as needed
+        response = response[:500_000]
+
+        # Get the Pygments formatter
+        formatter = HtmlFormatter(style="colorful")
+
+        # Highlight the data
+        response = highlight(response, JsonLexer(), formatter)
+
+        # Get the stylesheet
+        style = "<style>" + formatter.get_style_defs() + "</style>"
+
+        # Safe the output
+        return SafeString(style + response)
+
+    @admin.display(description="Data")
+    def data_excerpt(self, obj):
+        if len(str(obj.data)) > 127:
+            ret = str(obj.data)[:127] + "..."
+        else:
+            ret = str(obj.data)
+        return SafeString("<code>" + ret + "</code>")
 
 
 class HeaderInline(admin.TabularInline):
